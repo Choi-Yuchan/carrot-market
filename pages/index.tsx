f38493e262ae65b603fdb562baa9954d42babd40
Logan_YC_Choi <logan_yc_choi@Logan-YC-Choiui-MacBookPro.local>
2022-09-13 22:09:02 +0900

    adapt transitions to color buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,9 +57,9 @@ const Home: NextPage = () => {
           <span className='text-xs text-gray-500'>Chair</span>
           <div className='mt-3 mb-5 flex justify-between items-center'>
             <div className='flex justify-between items-center space-x-3'>
-              <button className='w-5 h-5 rounded-full bg-yellow-500' />
-              <button className='w-5 h-5 rounded-full bg-indigo-500' />
-              <button className='w-5 h-5 rounded-full bg-teal-500' />
+              <button className='w-5 h-5 rounded-full transition bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 ring-opacity-50' />
+              <button className='w-5 h-5 rounded-full transition bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 ring-opacity-50' />
+              <button className='w-5 h-5 rounded-full transition bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 ring-opacity-50' />
             </div>
             <div className='flex items-center space-x-5'>
               <button className='p-1.5 bg-blue-100 rounded-lg flex justify-center items-center aspect-square w-8 text-xl text-gray-600'>-</button>
